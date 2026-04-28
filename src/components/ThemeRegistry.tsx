25a6bab9f52327e5ded0b1f303505bc6d01bc648
TimmyGray <timmy081290@gmail.com>
2026-04-28 16:58:17 +0300

    Updated ThemeRegistry.tsx to use AppRouterCacheProvider from @mui/material-nextjs/v16-appRouter. Wrapped the existing ThemeProvider and CssBaseline inside AppRouterCacheProvider so SSR and client generate matching Emotion style tags.

--- src/components/ThemeRegistry.tsx
+++ src/components/ThemeRegistry.tsx
@@ -3,6 +3,7 @@
 import * as React from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v16-appRouter';
 
 const theme = createTheme({
   palette: {
@@ -84,9 +85,11 @@ const theme = createTheme({
 
 export default function ThemeRegistry({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      {children}
-    </ThemeProvider>
+    <AppRouterCacheProvider options={{ key: 'mui' }}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </AppRouterCacheProvider>
   );
 }
